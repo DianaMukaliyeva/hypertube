39dd134dfa4182dbe5bb2b1de2474e969a44f667
iljaSL <iljasmelich@gmail.com>
2021-02-04 22:15:02 +0200

    added eslinter to the backend and a gitignore file

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // connect to Mongo daemon
 mongoose
-  .connect(`mongodb://mongo:27017/hypertube`, {
+  .connect('mongodb://mongo:27017/hypertube', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
