445c83fc5ee87afd996744c22e0169b4187ae5ea
Esa Halmkrona <ehalmkro@student.hive.fi>
2021-02-06 13:01:29 +0200

    fix routing, add initial movielist query

--- backend/utilities/fetchMovieList.js
+++ backend/utilities/fetchMovieList.js
@@ -5,7 +5,7 @@ export const fetchYTSMovieList = async (page = 1) => {
     const { OMDB_KEY, TORRENT_API } = process.env;
 
     let res = await axios
-        .get(`${TORRENT_API}?sort_by=download_count&page=${page}&limit=${limit}`)
+        .get(`${TORRENT_API}?sort_by=year&page=${page}&limit=${limit}&`)
     if (res.status !== 200)
         throw new Error('Could not fetch movie list!');
     if (!res.data.data.movies)
@@ -17,7 +17,7 @@ export const fetchYTSMovieList = async (page = 1) => {
         if (res.status !== 200)
             throw new Error(`Could not fetch movie data from ${movie.imdb_code}!`);
         movies = [...movies, {imdb_code: movie.imdb_code,
-            magnet: `magnet:?xt=urn:btih:${movie.torrents[0].hash}&dn=Url+Encoded+Movie+Name&tr=http://track.one:1234/announce&tr=udp://track.two:80`,
+            hash: `${movie.torrents[0].hash}`,
             omdb_data: res.data}]
     }))
     return movies;
