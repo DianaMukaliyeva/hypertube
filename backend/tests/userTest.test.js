040dc7df6b328b93243b242b02b7828a4e7b1d46
iljaSL <iljasmelich@gmail.com>
2021-02-08 21:22:54 +0200

    create jest tests and github actions

---
+++ backend/tests/userTest.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import supertest from 'supertest';
+import app from '../app';
+
+const request = supertest(app);
+
+describe('User API tests', () => {
+  test('test okay', async () => {
+    await request.get('/test').expect(200);
+  });
+});
+
+afterAll(async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+});
