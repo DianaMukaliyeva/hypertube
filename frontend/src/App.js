bf3bc359755f768bbceb0260f34de3f5c1dc3dc1
Tatiana Kuumola <tkuumola@gmail.com>
2021-02-09 19:37:54 +0200

    build: :hammer: add material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,22 @@ import Navigation from './components/layout/Navigation';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './styles/theme';
+
 function App() {
   return (
-    <Router>
-      <Container>
-        <Navigation></Navigation>
-        <Route exact path="/" component={Landing}></Route>
-        <Switch>
-          <Route exact path="/hypertube" component={Hypertube} />
-        </Switch>
-      </Container>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Container>
+          <Navigation></Navigation>
+          <Route exact path="/" component={Landing}></Route>
+          <Switch>
+            <Route exact path="/hypertube" component={Hypertube} />
+          </Switch>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
 }
 
