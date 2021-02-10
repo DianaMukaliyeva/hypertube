23ee28562b07d4b8bebf7c363b82b7d133e4b5fd
Esa Halmkrona <ehalmkro@student.hive.fi>
2021-02-10 20:13:15 +0200

    fix routing according to spec, modify docs accordingly

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,7 +1,7 @@
 import movieListUtils from '../utilities/movieAPIUtilities.js';
 import Movie from '../models/Movie.js';
 
-const getMovieList = async (req, res, next) => {
+const getMovieList = async (req, res, next) => { // todo: add parameters
   try {
     const page = req.query.page || 1;
     res.json(await movieListUtils.fetchYTSMovieList(page));
@@ -21,28 +21,9 @@ const addMovieToDb = async (req, res, next) => {
 const getMovieEntry = async (req, res, next) => {
   try {
     const imdbCode = req.params.imdb_code;
-    const torrentData = await movieListUtils.fetchTorrentData(imdbCode); // TODO: add error handling
+    const torrentData = await movieListUtils.fetchTorrentData(imdbCode);
     const movieInfo = await movieListUtils.fetchMovieInfo(imdbCode);
-    const { hash } = torrentData.movies[0].torrents[0];
-    const magnet = `magnet:?xt=urn:btih:${hash}&dn=${movieInfo.Title}tr=http://track.one:1234/announce&tr=udp://track.two:80`;
-
-    // this response needs to be moved to a separate parser function in utilities
-    res.json({
-      title: movieInfo.Title,
-      imdbCode,
-      imdbRating: torrentData.movies[0].rating,
-      year: movieInfo.Year,
-      genre: movieInfo.Genre, // need to parse if we only want one
-      description: movieInfo.Plot,
-      length: movieInfo.Runtime,
-      director: movieInfo.Director,
-      cast: movieInfo.Actors.split(/, +/),
-      subtitles: ['PLACEHOLDER'], // todo: investigate API for this
-      comments: ['PLACEHOLDER'], // todo: add functionality
-      downloaded: false, // if movie is already downloaded to server, identified with imdbCode
-      watched: false, // to see if user has already watched the movie, identified with imdbCode
-      magnet,
-    });
+    res.json(movieListUtils.parseMovieResponse(movieInfo, torrentData));
   } catch (err) { next(err); }
 };
 
--- backend/utilities/movieAPIUtilities.js
+++ backend/utilities/movieAPIUtilities.js
@@ -1,47 +1,74 @@
 import axios from 'axios';
 
+const requestError = new Error('Could not fetch movie data'); // todo: specify these more explicitly in a separate file
+requestError.code = 'movieRequest';
+requestError.name = 'Request failed';
+
 const fetchYTSMovieList = async (page = 1) => {
   const limit = 24;
   const { OMDB_KEY, TORRENT_API } = process.env;
 
   let res = await axios
-    .get(`${TORRENT_API}?sort_by=year&page=${page}&limit=${limit}&`);
-  if (res.status !== 200) throw new Error('Could not fetch movie list!');
-  if (!res.data.data.movies) throw new Error('Page does not exist');
+    .get(`${TORRENT_API}?sort_by=download_count&page=${page}&limit=${limit}&`);
+  if (res.status !== 200) throw new Error('Could not fetch movie list!'); // todo: custom errors for these
+  if (!res.data.data.movies) throw new Error('Page does not exist'); // todo: custom errors for these
   const movieList = res.data.data.movies;
   let movies = [];
   await Promise.all(movieList.map(async (movie) => {
     res = await axios.get(`http://www.omdbapi.com/?i=${movie.imdb_code}&apikey=${OMDB_KEY}`);
-    if (res.status !== 200) throw new Error(`Could not fetch movie data from ${movie.imdb_code}!`);
+    if (res.status !== 200) throw requestError;
     movies = [...movies, {
       title: res.data.Title,
       imdbCode: movie.imdb_code,
       imdbRating: movie.rating,
-      year: res.data.Year,
+      year: parseInt(res.data.Year, 10),
       thumbnail: res.data.Poster,
       hash: `${movie.torrents[0].hash}`,
-      watched: false, // placeholder
+      watched: false, // placeholder until the backend logic is added
     }];
   }));
-  return movies;
+  return { movies };
 };
 
 const fetchTorrentData = async (imdbCode) => {
   const { TORRENT_API } = process.env;
   const res = await axios
     .get(`${TORRENT_API}?query_term=${imdbCode}`);
-  if (res.status !== 200 || !res.data.data) throw new Error(`Could not fetch movie entry with id ${imdbCode}!`);
+  if (res.status !== 200 || !res.data.data) throw requestError;
   return res.data.data;
 };
 
 const fetchMovieInfo = async (imdbCode) => {
   const { OMDB_KEY } = process.env;
   const res = await axios.get(`http://www.omdbapi.com/?i=${imdbCode}&apikey=${OMDB_KEY}`);
-  if (res.status !== 200) throw new Error(`Could not fetch movie data from ${imdbCode}!`);
+  if (res.status !== 200) throw requestError;
   return res.data;
 };
+
+const parseMovieResponse = (movieInfo, torrentData) => (
+
+  /* storing magnet link components here for now as we might need them in movie routes later, sry.
+  also todo: add tracker listing into a config file
+ const { hash } = torrentData.movies[0].torrents[0];
+ `magnet:?xt=urn:btih:${hash}&dn=${movieInfo.Title}&tr=[PLACEHOLDER_FOR_TRACKER]`; */
+
+  {
+    title: movieInfo.Title,
+    imdbRating: torrentData.movies[0].rating,
+    year: movieInfo.Year,
+    genre: movieInfo.Genre, // need to split this if we only want one
+    description: movieInfo.Plot,
+    length: parseInt(movieInfo.Runtime, 10),
+    director: movieInfo.Director,
+    cast: movieInfo.Actors.split(', '),
+    subtitles: [{ en: 'placeholder subtitle object' }], // todo: investigate API for this
+    downloaded: false, // if movie is already downloaded to server, identified with imdbCode
+    watched: false, // to see if user has already watched the movie, identified with imdbCode
+    comments: [{ userId: 123, username: 'Nina Lustig', message: 'Placeholder comment' }], // todo: add functionality
+  });
 export default {
   fetchYTSMovieList,
   fetchTorrentData,
   fetchMovieInfo,
+  parseMovieResponse,
 };
