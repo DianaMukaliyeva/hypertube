aa1045e89519e52b830236fb04a9655bf3192571
iljaSL <iljasmelich@gmail.com>
2021-02-11 21:58:00 +0200

    starting with building video player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 import Landing from './components/layout/Landing';
 import Hypertube from './components/gallery/Index';
 import Navigation from './components/layout/Navigation';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={Landing}></Route>
         <Switch>
           <Route exact path="/hypertube" component={Hypertube} />
+          <Route exact path="/video-player" component={VideoPlayer} />
         </Switch>
       </Container>
     </Router>
