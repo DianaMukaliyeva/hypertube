b4cc566a635dccdcbfd94adbac65e344b06e2be2
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-02-11 23:38:14 +0200

    disabel eslint for process

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
 
-const baseUrl = process.env.REACT_APP_BACKEND_URL;
+const baseUrl = process.env.REACT_APP_BACKEND_URL; // eslint-disable-line no-undef
 
 const auth = async () => {
   // console.log('service > auth > auth');
