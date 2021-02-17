8533b797045fa6227f7998fa07d03dfe65aa3e7f
Tatiana Kuumola <tkuumola@gmail.com>
2021-02-17 22:53:30 +0200

    feat: :sparkles: add showError function

---
+++ frontend/src/utils/sharedFunctions.js
@@ -0,0 +1,41 @@
+const sharedFunctions = {};
+
+sharedFunctions.showErrors = (errors, params) => {
+  console.log(errors, params);
+  errors.map((detail) => {
+    if (detail.param === 'username') {
+      params.username.setValues({
+        ...params.username.values,
+        helperText: detail.reason,
+        error: true,
+      });
+    } else if (detail.param === 'firstName') {
+      params.firstName.setValues({
+        ...params.firstName.values,
+        helperText: detail.reason,
+        error: true,
+      });
+    } else if (detail.param === 'lastName') {
+      params.lastName.setValues({
+        ...params.lastName.values,
+        helperText: detail.reason,
+        error: true,
+      });
+    } else if (detail.param === 'email') {
+      params.email.setValues({ ...params.email.values, helperText: detail.reason, error: true });
+    } else if (detail.param === 'password') {
+      params.password.setValues({
+        ...params.password.values,
+        helperText: detail.reason,
+        error: true,
+      });
+    } else if (detail.param === 'confirmPassword') {
+      params.confirmPassword.setValues({
+        ...params.confirmPassword.values,
+        helperText: detail.reason,
+        error: true,
+      });
+    }
+  });
+};
+export default sharedFunctions;
