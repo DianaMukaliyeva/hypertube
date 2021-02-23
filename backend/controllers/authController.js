d764c65537b35651589e53d7a5e54cca7a7d4fc8
Tatiana Kuumola <tkuumola@gmail.com>
2021-02-23 21:45:17 +0200

    feat: :sparkles: add recovery link controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import sendResetEmail from '../utilities/email.js';
 
 const login = async (req, res) => {
   const { email } = req.body;
@@ -21,7 +22,19 @@ const test = async (req, res) => {
   res.status(200).json('success');
 };
 
-const recoveryEmail = async (req, res) => {};
+const recoveryEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+  const userForToken = {
+    id: user.id,
+    lang: user.language,
+  };
+  const token = jwt.sign(userForToken, process.env.SECRET);
+  const temp = sendResetEmail(user.email, user.firstname, token, req);
+
+  res.status(200).json('success');
+};
 
 export default {
   login,
