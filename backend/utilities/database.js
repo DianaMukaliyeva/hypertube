f851740fba8fcd86712854017c45502921d80621
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-02-28 22:24:34 +0200

    test: add test database

--- backend/utilities/database.js
+++ backend/utilities/database.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose';
 
 const connect = () => {
-  mongoose.connect('mongodb://mongo:27017/hypertube', {
+  const database = process.env.NODE_ENV === 'test' ? 'test' : 'hypertube';
+  mongoose.connect(`mongodb://mongo:27017/${database}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   });
 };
 
-export default { connect };
+const closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+};
+
+export default { connect, closeDatabase };
