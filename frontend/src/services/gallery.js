5c503a445fa09c6cc4484110dd141069f45bdf7a
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-03-05 00:50:17 +0200

    feat: :sparkles: implement frontend filter

--- frontend/src/services/gallery.js
+++ frontend/src/services/gallery.js
@@ -3,8 +3,22 @@ import axios from 'axios';
 // eslint-disable-next-line no-undef
 const baseUrl = process.env.REACT_APP_BACKEND_URL + '/api/movies';
 
-const getMovies = async (page) => {
-  const res = await axios.get(baseUrl + `?page=${page}`);
+const generateQueryParams = (filter) => {
+  const params = Object.keys(filter).reduce((result, key) => {
+    if (key === 'sort' && filter[key]) {
+      const sorts = filter[key].split(' ');
+      result += `&sort_by=${sorts[0]}&order_by=${sorts[1]}`;
+    } else {
+      result += filter[key] ? `&${key}=${filter[key]}` : '';
+    }
+    return result;
+  }, '');
+  return params;
+};
+
+const getMovies = async (page, filter, search) => {
+  const params = generateQueryParams(filter);
+  const res = await axios.get(baseUrl + `?page=${page}&search=${search}${params}`);
   return res.data;
 };
 
