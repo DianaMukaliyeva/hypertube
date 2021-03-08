e6853358727514816341825f2dc39f576e556ad9
llahti <llahti@student.hive.fi>
2021-03-08 21:34:53 +0200

    Create omniAuthLogin with logo buttons

---
+++ frontend/src/components/user/OmniAuthLogin.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import authService from '../../services/auth';
+import LogoButton from './LogoButton';
+import GoogleLogo from '../../images/google-logo.png';
+import FortyTwoLogo from '../../images/42-logo.png';
+
+const OmniAuthLogin = () => {
+	const handleGoogleSignIn = async () => {
+    try {
+      const data = await authService.googleUrl();
+      window.location = data.url;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handle42SignIn = async () => {
+    try {
+      const data = await authService.fortytwoUrl();
+      window.location = data.url;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      <LogoButton
+        handleClick={handleGoogleSignIn}
+        name="Login with Google"
+        logo={GoogleLogo}
+      />
+      <LogoButton
+        handleClick={handle42SignIn}
+        name="Login with 42"
+        logo={FortyTwoLogo}
+      />
+    </>
+  );
+};
+
+
+export default OmniAuthLogin;
