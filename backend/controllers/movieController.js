9f9d58313f67fec4f0eafb60861bb95196d08847
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-03-09 16:57:58 +0200

    feat: :sparkles: add filter to get movies on backend

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,13 +1,26 @@
 import movieListUtils from '../utilities/movieAPIUtilities.js';
 import Movie from '../models/Movie.js';
+import User from '../models/User.js';
 
 const getMovieList = async (req, res) => {
-  const { page = 1, ...filters } = req.query;
-  const movies = await movieListUtils.fetchYTSMovieList(page, filters);
+  const userId = req.user;
+  const filters = req.query;
+  const movies = await movieListUtils.fetchYTSMovieList(filters);
+  const user = await User.findById(userId);
+
+  movies.movies = movies.movies.map((movie) => {
+    const tempMovie = { ...movie };
+    user.watched.forEach((watched) => {
+      if (watched.movieId === movie.imdbCode) {
+        tempMovie.watched = true;
+      }
+    });
+    return tempMovie;
+  });
   res.json(movies);
 };
 
-const addMovieToDb = async (req, res, next) => {
+const addMovieToDb = async (req, res) => {
   const newMovie = new Movie({
     serverLocation: req.body.location,
   });
@@ -15,7 +28,7 @@ const addMovieToDb = async (req, res, next) => {
   res.sendStatus(201);
 };
 
-const getMovieEntry = async (req, res, next) => {
+const getMovieEntry = async (req, res) => {
   const imdbCode = req.params.imdb_code;
   const torrentData = await movieListUtils.fetchTorrentData(imdbCode);
   const movieInfo = await movieListUtils.fetchMovieInfo(imdbCode);
