2cfd439e7eb72379f47471ca491ec39713e4d004
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-03-09 16:57:58 +0200

    feat: :sparkles: add filter to get movies on backend

--- backend/utilities/movieAPIUtilities.js
+++ backend/utilities/movieAPIUtilities.js
@@ -1,49 +1,70 @@
 import axios from 'axios';
 import Movie from '../models/Movie.js';
+import { notFoundError } from './errors.js';
 
+const { OMDB_KEY, TORRENT_API } = process.env;
 const requestError = new Error('Could not fetch movie data'); // todo: specify these more explicitly in a separate file
 requestError.code = 'movieRequest';
 requestError.name = 'Request failed';
 
-const fetchYTSMovieList = async (page = 1, filters) => {
-  const limit = 24;
-  const { OMDB_KEY, TORRENT_API } = process.env;
-
-  let res = await axios.get(`${TORRENT_API}?sort_by=download_count&page=${page}&limit=${limit}&`);
-  if (res.status !== 200) throw new Error('Could not fetch movie list!'); // todo: custom errors for these
-  if (!res.data.data.movies) throw new Error('Page does not exist'); // todo: custom errors for these
-  const movieList = res.data.data.movies;
+const fetchYTSMovieList = async (filters) => {
   let movies = [];
-  await Promise.all(
-    movieList.map(async (movie, index) => {
-      res = await axios.get(`http://www.omdbapi.com/?i=${movie.imdb_code}&apikey=${OMDB_KEY}`);
-      if (res.status !== 200) throw requestError;
-      movies = [
-        ...movies,
-        {
-          title: res.data.Title,
-          imdbCode: movie.imdb_code,
-          imdbRating: movie.rating,
-          year: parseInt(res.data.Year, 10),
-          thumbnail: res.data.Poster,
-          hash: `${movie.torrents[0].hash}`,
-          watched: false, // placeholder until the backend logic is added
-        },
-      ];
-    }),
-  );
-  return { movies };
+  let hasMore = true;
+  const params = {
+    limit: 20,
+    page: filters.page || 1,
+    minimum_rating: filters.imdb || 0,
+    genre: filters.genre,
+    sort_by: filters.sort_by,
+    order_by: filters.order_by,
+    query_term: filters.search || '',
+  };
+
+  try {
+    let res = await axios.get(`${TORRENT_API}`, { params });
+    const moviesData = res.data.data;
+    const movieList = res.data.data.movies;
+
+    if (moviesData.movie_count === 0) {
+      throw notFoundError();
+    }
+    if (moviesData.movie_count <= params.limit * params.page) {
+      hasMore = false;
+    }
+    if (movieList) {
+      await Promise.all(
+        movieList.map(async (movie) => {
+          res = await axios.get(`http://www.omdbapi.com/?i=${movie.imdb_code}&apikey=${OMDB_KEY}`);
+          movies = [
+            ...movies,
+            {
+              title: movie.title,
+              imdbCode: movie.imdb_code,
+              imdbRating: movie.rating,
+              year: movie.year,
+              thumbnail: res.data.Poster,
+              hash: movie.torrents[0].hash,
+            },
+          ];
+        }),
+      );
+    }
+  } catch (e) {
+    if (e && e.statusCode === 404) {
+      return { movies: [], hasMore: false };
+    }
+    throw e;
+  }
+  return { movies, hasMore };
 };
 
 const fetchTorrentData = async (imdbCode) => {
-  const { TORRENT_API } = process.env;
   const res = await axios.get(`${TORRENT_API}?query_term=${imdbCode}`);
   if (res.status !== 200 || !res.data.data) throw requestError;
   return res.data.data;
 };
 
 const fetchMovieInfo = async (imdbCode) => {
-  const { OMDB_KEY } = process.env;
   const res = await axios.get(`http://www.omdbapi.com/?i=${imdbCode}&apikey=${OMDB_KEY}`);
   if (res.status !== 200) throw requestError;
   return res.data;
