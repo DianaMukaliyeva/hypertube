7b1ad1bb44fc9a7ca3436b04bfb2ecf5c355dfa6
Tatiana Kuumola <tkuumola@gmail.com>
2021-03-09 22:35:22 +0200

    feat: :sparkles: add subtitles route & controller

---
+++ backend/controllers/subtitlesController.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+const getSubtiles = async (req, res) => {
+  const { imdbCode, lang } = req.params;
+  const filename = `./subtitles/${imdbCode}/${lang}/subtitle.vtt`;
+
+  let readStream = fs.createReadStream(filename);
+
+  readStream.on('open', function () {
+    const head = {
+      'Content-Type': 'text/vtt',
+    };
+    res.writeHead(200, head);
+    readStream.pipe(res);
+  });
+  readStream.on('error', function (err) {
+    res.end(err);
+  });
+};
+
+export default {
+  getSubtiles,
+};
