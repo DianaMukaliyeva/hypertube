226ecda7a334d74fb88007a421abf30f911f9591
llahti <llahti@student.hive.fi>
2021-03-12 15:05:49 +0200

    Create useField tests

---
+++ frontend/src/hooks/useField.test.js
@@ -0,0 +1,67 @@
+//import React from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { waitFor } from '@testing-library/react';
+import useField from './useField';
+
+const validEmail = 'hilla@halla.fi';
+const invalidEmail = 'hilla';
+const invalidEmail2 = 'hilla@';
+
+describe('calling onChange changes value and helper text', () => {
+  it('valid email shows correct helper text', async () => {
+    const { result } = renderHook(() =>
+      useField('email', 'email', 'test-email')
+    );
+
+    expect(result.current.value).toBe('');
+
+    act(() => {
+      result.current.onChange({
+        target: {
+          value: validEmail,
+        },
+      });
+    });
+
+    await waitFor(() => expect(result.current.value).toBe(validEmail));
+    expect(result.current.helperText).toBe('form.correct');
+  });
+
+  it('invalid email shows error helper text', async () => {
+    const { result } = renderHook(() =>
+      useField('email', 'email', 'test-email')
+    );
+
+    expect(result.current.value).toBe('');
+
+    act(() => {
+      result.current.onChange({
+        target: {
+          value: invalidEmail,
+        },
+      });
+    });
+
+    await waitFor(() => expect(result.current.value).toBe(invalidEmail));
+    expect(result.current.helperText).toContain('form.email');
+  });
+
+  it('invalid email shows error helper text', async () => {
+    const { result } = renderHook(() =>
+      useField('email', 'email', 'test-email')
+    );
+
+    expect(result.current.value).toBe('');
+
+    act(() => {
+      result.current.onChange({
+        target: {
+          value: invalidEmail2,
+        },
+      });
+    });
+
+    await waitFor(() => expect(result.current.value).toBe(invalidEmail2));
+    expect(result.current.helperText).toContain('form.email');
+  });
+});
