f7a365eb71ee3f419661fb07811b4eca278357ea
Tatiana Kuumola <tkuumola@gmail.com>
2021-03-17 18:21:33 +0200

    style: :sparkles: experimenting with landing

--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
@@ -7,12 +8,14 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import useModal from '../../hooks/useModal';
 import CustomModal from '../common/CustomModal';
 import LoginForm from '../auth/LoginForm';
 import CreateAccountForm from '../auth/CreateAccountForm';
 import PasswordResetForm from '../auth/PasswordResetForm';
+import sample from './sample.mp4';
 
 // TO DO move to styles
 const useStyles = makeStyles((theme) => ({
@@ -49,25 +52,69 @@ const Landing = ({ user, setUser }) => {
   const loginModal = useModal(<LoginForm setUser={setUser} />);
   const createAccountModal = useModal(<CreateAccountForm setUser={setUser} />);
 
+  const Hyper = () => {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100vh',
+          overflow: 'hidden',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <video
+          autoPlay
+          muted
+          loop
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}>
+          <source src={sample} type="video/mp4"></source>
+        </video>
+        <h2
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            background: '#fff',
+            fontSize: '70vh',
+            textAlign: 'center',
+            textTransform: 'uppercase',
+            color: '#000',
+            lineHeight: '100vh',
+            mixBlendMode: 'screen',
+          }}>
+          Hyper
+        </h2>
+      </div>
+    );
+  };
+
+  const clouds =
+    'https://www.youtube.com/embed/bog4VzMWP20?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&playlist=bog4VzMWP20';
+
   return (
     <div style={{ marginTop: '25%', textAlign: 'center' }}>
       <Typography variant="h1">HYPERTUBE</Typography>
       {!user.userId && (
         <>
-          <Box
-            mt={5}
-            display="flex"
-            justifyContent="center"
-            className={classes.box}
-          >
+          <Box mt={5} display="flex" justifyContent="center" className={classes.box}>
             <Box m={3}>
               <Button
                 className={classes.button}
                 type="submit"
                 variant="outlined"
                 color="primary"
-                onClick={loginModal.handleClickOpen}
-              >
+                onClick={loginModal.handleClickOpen}>
                 {t('login.login')}
               </Button>
             </Box>
@@ -77,8 +124,7 @@ const Landing = ({ user, setUser }) => {
                 type="submit"
                 variant="outlined"
                 color="secondary"
-                onClick={createAccountModal.handleClickOpen}
-              >
+                onClick={createAccountModal.handleClickOpen}>
                 {t('createAccount.create')}
               </Button>
             </Box>
@@ -90,6 +136,41 @@ const Landing = ({ user, setUser }) => {
       <CustomModal {...passwordResetModal} />
     </div>
   );
+
+  // return (
+  //   <div style={{ marginTop: '25%', textAlign: 'center' }}>
+  //     <Typography variant="h1">HYPERTUBE</Typography>
+  //     {!user.userId && (
+  //       <>
+  //         <Box mt={5} display="flex" justifyContent="center" className={classes.box}>
+  //           <Box m={3}>
+  //             <Button
+  //               className={classes.button}
+  //               type="submit"
+  //               variant="outlined"
+  //               color="primary"
+  //               onClick={loginModal.handleClickOpen}>
+  //               Login
+  //             </Button>
+  //           </Box>
+  //           <Box m={3}>
+  //             <Button
+  //               className={classes.button}
+  //               type="submit"
+  //               variant="outlined"
+  //               color="secondary"
+  //               onClick={createAccountModal.handleClickOpen}>
+  //               Create Account
+  //             </Button>
+  //           </Box>
+  //         </Box>
+  //       </>
+  //     )}
+  //     <CustomModal {...loginModal} />
+  //     <CustomModal {...createAccountModal} />
+  //     <CustomModal {...passwordResetModal} />
+  //   </div>
+  // );
 };
 
 Landing.propTypes = {
