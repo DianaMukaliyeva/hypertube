3bf7ec07427adcef2f3152d2f2b21adfedc55776
llahti <llahti@student.hive.fi>
2021-03-18 09:13:34 +0200

    Remove UserProfile example from MyProfile

--- frontend/src/components/profile/MyProfile/index.js
+++ frontend/src/components/profile/MyProfile/index.js
@@ -11,14 +11,8 @@ import useField from '../../../hooks/useField';
 import userService from '../../../services/user';
 import sharedFunctions from '../../../utils/sharedFunctions';
 import UpdatePhoto from './UpdatePhoto';
-import UserProfile from '../UserProfile';
 import UpdatePassword from './UpdatePassword';
 import UpdateInformation from './UpdateInformation';
-
-// todo remove when done
-import Button from '@material-ui/core/Button';
-import CustomModal from '../../common/CustomModal';
-import useModal from '../../../hooks/useModal';
 
 // TO DO move to styles
 const useStyles = makeStyles((theme) => ({
@@ -68,9 +62,6 @@ const MyProfile = ({ user, setUser }) => {
   const [passwordAlert, setPasswordAlert] = useState(noAlert);
   const classes = useStyles();
   const { t } = useTranslation();
-
-  // todo: remove when done
-  const userProfileModal = useModal(<UserProfile user={userData} />);
 
   const handleErrorResponse = (data, showAlert) => {
     switch (data.statusCode) {
@@ -180,23 +171,12 @@ const MyProfile = ({ user, setUser }) => {
       <Grid container spacing={6}>
         <Grid item xs={12} sm={5} className={classes.column}>
           <UpdatePhoto avatar={avatar} setAvatar={setAvatar} />
-
-          <Button
-            type="submit"
-            variant="outlined"
-            color="primary"
-            onClick={userProfileModal.handleClickOpen}
-          >
-            User Profile
-          </Button>
-          <p>(this button will be removed)</p>
         </Grid>
         <Grid item xs={12} sm={7} className={classes.column}>
           <UpdateInformation {...updateInformationProps} />
-          <UpdatePassword {...updatePasswordProps} />
+          {userData.hasPw && <UpdatePassword {...updatePasswordProps} />}
         </Grid>
       </Grid>
-      <CustomModal {...userProfileModal} />
     </Container>
   ) : null;
 };
