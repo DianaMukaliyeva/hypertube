7dba0f7cccf7a7a1f291259e91664b328fc7e7bb
llahti <llahti@student.hive.fi>
2021-03-19 14:56:19 +0200

    Add UpdatePassword form tests

--- frontend/src/components/profile/MyProfile/UpdatePassword.test.js
+++ frontend/src/components/profile/MyProfile/UpdatePassword.test.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { renderHook } from '@testing-library/react-hooks';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import UpdatePassword from './UpdatePassword';
-import useField from '../../../hooks/useField';
 
-const oldPassword = renderHook(() =>
-  useField('password', 'password', 'update-old-password')
-);
-const password = renderHook(() =>
-  useField('password', 'password', 'update-password')
-);
-const confirmPassword = renderHook(() =>
-  useField('password', 'confirmPassword', 'update-confirm-pw')
-);
+const validPassword = 'Salasana5';
+const validNewPassword = 'Sala6Sana';
+const invalidNewPassword = 'sala1sana';
 
 const props = {
   classes: {
     form: 'form',
     alert: 'alert',
   },
-  oldPassword: oldPassword.result.current,
-  password: password.result.current,
-  confirmPassword: confirmPassword.result.current,
   alert: {
     show: false,
     message: '',
@@ -50,5 +40,83 @@ describe('all components are rendered', () => {
   it('renders update password button', () => {
     render(<UpdatePassword {...props} />);
     screen.getByRole('button');
+  });
+
+  it('does not render alert', () => {
+    render(<UpdatePassword {...props} />);
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+  });
+});
+
+describe('form is submitted', () => {
+  it('handles submit with valid values', async () => {
+    render(<UpdatePassword {...props} />);
+
+    userEvent.paste(screen.getByLabelText(/currentPassword/i), validPassword);
+    await screen.findByDisplayValue(validPassword);
+
+    userEvent.paste(screen.getByLabelText(/newPassword/i), validNewPassword);
+    await screen.findByDisplayValue(validNewPassword);
+
+    userEvent.paste(
+      screen.getByLabelText(/confirmPassword/i),
+      validNewPassword
+    );
+    await waitFor(() => {
+      expect(screen.getAllByDisplayValue(validNewPassword)).toHaveLength(2);
+    });
+
+    userEvent.click(screen.getByRole('button', { name: /update/i }));
+    expect(props.handleUpdate).toHaveBeenCalled();
+    expect(props.handleUpdate).toHaveBeenCalledWith(
+      {
+        oldPassword: validPassword,
+        password: validNewPassword,
+        confirmPassword: validNewPassword,
+      },
+      expect.anything(),
+      expect.anything()
+    );
+  });
+
+  it('handles submit with invalid values', async () => {
+    render(<UpdatePassword {...props} />);
+
+    userEvent.paste(screen.getByLabelText(/currentPassword/i), validPassword);
+    await screen.findByDisplayValue(validPassword);
+
+    userEvent.paste(screen.getByLabelText(/newPassword/i), invalidNewPassword);
+    await screen.findByDisplayValue(invalidNewPassword);
+
+    userEvent.paste(
+      screen.getByLabelText(/confirmPassword/i),
+      validNewPassword
+    );
+    await screen.findByDisplayValue(validNewPassword);
+
+    userEvent.click(screen.getByRole('button', { name: /update/i }));
+    expect(props.handleUpdate).toHaveBeenCalled();
+    expect(props.handleUpdate).toHaveBeenCalledWith(
+      {
+        oldPassword: validPassword,
+        password: invalidNewPassword,
+        confirmPassword: validNewPassword,
+      },
+      expect.anything(),
+      expect.anything()
+    );
+  });
+
+  it('handles submit with missing values', async () => {
+    render(<UpdatePassword {...props} />);
+
+    userEvent.paste(screen.getByLabelText(/currentPassword/i), validPassword);
+    await screen.findByDisplayValue(validPassword);
+
+    userEvent.paste(screen.getByLabelText(/newPassword/i), validNewPassword);
+    await screen.findByDisplayValue(validNewPassword);
+
+    userEvent.click(screen.getByRole('button', { name: /update/i }));
+    expect(props.handleUpdate).not.toHaveBeenCalled();
   });
 });
