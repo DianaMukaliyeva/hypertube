8a61670500c1c60a627487ac599869534f0aab34
Tatiana Kuumola <tkuumola@gmail.com>
2021-03-20 00:24:09 +0200

    feat: :sparkles: add get comment service

--- frontend/src/services/movie.js
+++ frontend/src/services/movie.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 // eslint-disable-next-line no-undef
 const baseUrl = process.env.REACT_APP_BACKEND_URL + '/api/movies';
 
-const createComment = async (comment, imdbCode) => {
-  const data = {
-    comment,
-  };
+const createComment = async (data, imdbCode) => {
   const res = await axios.post(`${baseUrl}/${imdbCode}/comments`, data);
+
+  return res.data;
+};
+
+const getComments = async (imdbCode) => {
+  const res = await axios.get(`${baseUrl}/${imdbCode}/comments`);
 
   return res.data;
 };
@@ -28,4 +31,5 @@ export default {
   createComment,
   getMovieData,
   setWatched,
+  getComments,
 };
