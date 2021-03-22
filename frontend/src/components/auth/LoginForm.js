933af87dd94cf9c80c7c55bc730d3fea2b7daac7
Tatiana Kuumola <tkuumola@gmail.com>
2021-03-22 22:28:52 +0200

    refactor: :recycle: refactoring

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -44,6 +44,7 @@ const LoginForm = ({ setUser }) => {
   const email = useField('email', 'loginEmail', 'login-email');
   const password = useField('password', 'loginPassword', 'login-password');
   const alert = useAlert();
+  const classes = useStyles();
 
   const recoveryLinkModal = useModal(<RecoveryLinkForm />);
 
